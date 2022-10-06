97829119340872df212d04231eca9244e48ce986
Caro Barreiro <caro.barreiro.v@gmail.com>
2022-10-06 15:59:26 -0500

    filling all components with html

---
+++ assets/components/template.js
@@ -0,0 +1,10 @@
+//component wrapper - used avoid to polluting global namespace
+(function(){
+
+    //START COMPONENT
+    var html = ``;
+    var component = document.getElementById('component');
+    component.innerHTML = html;
+    //END COMPONENT
+    //close component wrapper
+})();
